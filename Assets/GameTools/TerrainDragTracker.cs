8fe9c6d775e6d5de498d12862f5f12ec195d231d
Gavin-McG <gavinmcg@umich.edu>
2026-05-19 19:01:11 -0400

    Drag Tracker class

---
+++ Assets/GameTools/TerrainDragTracker.cs
@@ -0,0 +1,60 @@
+﻿using Terrain;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace GameTools
+{
+    public class TerrainDragTracker
+    {
+        private PointerEventData cachedEventData;
+        private TerrainPointerInfo pressInfo;
+        private bool overTerrain;
+        private bool buttonHeld;
+
+        public bool IsDragging()
+        {
+            return overTerrain && cachedEventData != null;
+        }
+
+        public (TerrainPointerInfo min, TerrainPointerInfo max) GetDragBounds()
+        {
+            if (!IsDragging()) return default;
+
+            TerrainPointerInfo currentInfo = TerrainManager.GetRaycastInfo(cachedEventData.pointerCurrentRaycast);
+
+            // Hover only -> 1x1 preview
+            if (!buttonHeld)
+            {
+                return (currentInfo, currentInfo);
+            }
+
+            // Drag selection preview
+            return (pressInfo, currentInfo);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            cachedEventData = eventData;
+            pressInfo = TerrainManager.GetRaycastInfo(eventData.pointerCurrentRaycast);
+            buttonHeld = true;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            cachedEventData = eventData;
+            buttonHeld = false;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            cachedEventData = eventData;
+            overTerrain = true;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            cachedEventData = eventData;
+            overTerrain = false;
+        }
+    }
+}
